fc65d8e504f2795128b8b3948b3baf8a097f1f3d
Josephina Bos <josephinabos@gmail.com>
2016-11-10 16:13:56 +0100

    update movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :downloads
-  has_many :wishlist_movies
+  has_many :downloads, dependent: :destroy
+  has_many :wishlist_movies, dependent: :destroy
 
   validates :name, :duration, presence: :true
 end
