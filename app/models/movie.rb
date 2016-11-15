99f26293432ff7bee62b5547e0fd1673b83370c9
Josephina Bos <josephinabos@gmail.com>
2016-11-15 09:24:12 +0100

    complete genre and category functionality

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   has_many :downloads, dependent: :destroy
   has_many :wishlist_movies, dependent: :destroy
-  has_many :categories
+  has_many :categories, dependent: :destroy
   has_many :genres, through: :categories
 
   validates :name, :duration, presence: :true
